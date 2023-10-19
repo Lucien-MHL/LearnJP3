904daece94d46b5059de4b24d11034f8514c5591
Lucien <lucienengineerlife@gmail.com>
2023-10-19 21:52:48 +0800

    chore: add comment

--- src/elements/PageSection/index.tsx
+++ src/elements/PageSection/index.tsx
@@ -1,11 +1,13 @@
 import styled from 'styled-components'
 
 export const PageSection = styled.section`
+  /* 裝置寬度 > 850 時 */
   @media (min-width: 850px) {
     aspect-ratio: 4/3;
     width: 850px;
   }
 
+  /* 裝置寬度 < 850 時 */
   @media (max-width: 850px) {
     width: 100%;
     height: 80%;
