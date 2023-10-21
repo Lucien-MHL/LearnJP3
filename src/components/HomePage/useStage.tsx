4680776a370498463181ececeb5f9a2fba3e9ae0
Lucien <lucienengineerlife@gmail.com>
2023-10-21 14:24:15 +0800

    update: add few break line for readable

--- src/components/HomePage/useStage.tsx
+++ src/components/HomePage/useStage.tsx
@@ -12,9 +12,11 @@ interface IGroup {
 
 function useStage() {
   const keys = Object.keys(data)
+
   const character: string[] = keys
     .map(v => v.split('-')[0])
     .filter((v, i, a) => a.indexOf(v) === i)
+
   const stage: IItem[] = keys.map(key => {
     const [_, last] = key.split('-')
     return { text: last, dataKey: key }
